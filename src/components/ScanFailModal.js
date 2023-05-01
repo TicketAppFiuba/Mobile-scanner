ba22565e0994d508398cb8c61857b8dec6e24c6f
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-05-01 19:57:28 -0300

    Agrega detalle de error al escaneo fallido

--- src/components/ScanFailModal.js
+++ src/components/ScanFailModal.js
@@ -21,7 +21,7 @@ export default function ScanFailModal({ isVisible, closeModal, reason }) {
           <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', marginBottom: 10 }}>Entrada no escaneada con éxito</Text>
           <View style={{ marginBottom: 10 }}>
             <Text style={{ color: 'white', fontWeight: 'bold' }}>Detalles:</Text>
-            {/* <Text style={{ color: 'white' }}>{reason}</Text> */}
+            <Text style={{ color: 'white' }}>{reason}</Text>
           </View>
         </View>
       </View>
