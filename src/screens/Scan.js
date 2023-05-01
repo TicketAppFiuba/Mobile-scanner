733eb0ecbfce4e67892c5cd2cbffc0d7cbba7484
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-05-01 19:57:02 -0300

    Agrega logica de escaneo con backend

--- src/screens/Scan.js
+++ src/screens/Scan.js
@@ -1,18 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import ScanSuccessModal from '../components/ScanSuccessModal';
 import ScanFailModal from '../components/ScanFailModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('token')
+    if(value !== null) {
+      // value previously stored
+      return value
+    }
+  } catch(e) {
+    // error reading value
+  }
+}
 
-export default function App() {
+export default function App({route, navigation}) {
+  const { eventId, eventName } = route.params;
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+
+  const [token, setToken] = useState("");
 
   const [showSuccessModal, setShowSuccessModal] = useState(false);
   const [showFailModal, setShowFailModal] = useState(false);
   const [scannedData, setScannedData] = useState({nombre: '', apellido: '', email: ''});
   const [failReason, setFailReason] = useState();
+
+  const [lastScanTime, setLastScanTime] = useState(0);
+
+  useLayoutEffect(() => {
+    // get token from localstorage
+    getData()
+    .then((token) => {
+      setToken(token)
+    })
+  }, [])
+
+
+
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -26,11 +55,14 @@ export default function App() {
   const handleScanSuccess = (data) => {
     setScannedData(data);
     setShowSuccessModal(true);
+    setScanned(true);
   };
 
   const handleScanFail = (reason) => {
+    console.log("handleScanFail", reason)
     setFailReason(reason);
     setShowFailModal(true);
+
   };
 
   const handleCloseModal = () => {
@@ -42,20 +74,52 @@ export default function App() {
 
 
   const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setScannedData(data);
-    setShowSuccessModal(true);
+    const now = Date.now();
+
+    if (now - lastScanTime > 5000) {
+      fetch("https://71a7-201-212-239-28.ngrok-free.app/authorizer/ticket",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+          },
+          body: JSON.stringify({
+            "event_id": eventId, 
+            "reservation_code": data,
+            })
+      })
+      .then((response) => {
+        if (response.status != 200) {
+          response.json()
+          .then(json => handleScanFail(json.detail))
+        }
+        return response.json()
+      })
+      .then((json) => handleScanSuccess(json.detail))
+      .catch((error) => {
+        console.log("error: ", error)
+      })
+      setLastScanTime(now);
+    } 
   };
 
   if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+    return <Text style={{
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexGrow: 1,
+    }}>Solicitando Permisos de cámara</Text>;
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>No se tiene accesso a la cámara</Text>;
   }
 
   return (
     <View style={styles.container}>
+      <Text style={{fontSize: 20, paddingTop: 30, fontWeight: 'bold'}}>Escanear QR de {eventName}</Text>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
@@ -76,7 +140,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        flexGrow: 1,
     }
 
 }); 
