2dca7a9b4a298933bfaf5d0acd9fb1299988a075
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-05-26 14:17:37 -0300

    Agrega refresh en home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,148 +1,145 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import LoadingSpinner from '../components/LoadingSpinner';
-import { Button } from "react-native";
-
-
-
-import { ScrollView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity, RefreshControl } from 'react-native';
 import GetDayOfWeek from '../libs/DayOfWeek';
 
-
 const getData = async () => {
-    try {
-        const value = await AsyncStorage.getItem('token')
-        if (value !== null) {
-            // value previously stored
-            return value
-        }
-    } catch (e) {
-        // error reading value
+  try {
+    const value = await AsyncStorage.getItem('token');
+    if (value !== null) {
+      // value previously stored
+      return value;
     }
-}
-
-
-const handleEstadisticas = (navigation) => {
-    navigation.navigate('Statistics');
+  } catch (e) {
+    // error reading value
+  }
 };
 
 export default function Search({ navigation }) {
-    const [hasEvents, setHasEvents] = React.useState(false)
-    const [events, setEvents] = React.useState([])
+  const [hasEvents, setHasEvents] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
-    useLayoutEffect(() => {
-        getData()
-            .then((token) => {
+  const handleRefresh = () => {
+    setRefreshing(true);
+    // Fetch data again
+    getData()
+      .then((token) => {
+        fetch('https://b642-201-212-239-28.ngrok-free.app/authorizer/events', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+        })
+          .then((response) => response.json())
+          .then((json) => {
+            console.log('JSON events', json);
+            if (json.length === 0) {
+              setHasEvents(true);
+              setEvents([]);
+            } else {
+              setHasEvents(false);
+              setEvents(json);
+            }
+            setRefreshing(false);
+          })
+          .catch((error) => {
+            console.error('ERROR FETCH', error);
+            setRefreshing(false);
+          });
+      });
+  };
 
-                fetch("https://b697-181-29-197-107.sa.ngrok.io/authorizer/events", {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer ' + token
-                    }
-                })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        console.log("JSON events", json)
-                        if (json.length == 0) {
-                            setHasEvents(true)
-                            return
-                        }
-                        setEvents(json)
-                    })
-                    .catch((error) => {
-                        console.error("ERROR FETCH", error);
-                    })
-            })
-    }, [])
+  useLayoutEffect(() => {
+    handleRefresh();
+  }, []);
 
-
-
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Eventos</Text>
-            </View>
-            <View style={styles.body}>
-                <ScrollView style={{
-                    flex: 1,
-                    backgroundColor: '#fff',
-                    padding: 20
-
-                }}>
-                    {hasEvents ? <Text style={styles.noEventsText}>No hay eventos</Text> : null}
-                    {events.map((event) => {
-                        return (
-                            <TouchableOpacity style={styles.eventHeader} key={event.id} onPress={() => {
-                                navigation.navigate('Statistics', {
-                                    eventId: event.id,
-                                    eventName: event.title,
-                                    eventCapacity: event.capacity,
-                                    eventVacancies: event.vacancies
-                                })
-                            }}>
-                                <Text style={styles.eventTitle}>{event.title}</Text>
-                                <Text style={styles.eventDate}>{GetDayOfWeek(event.date)}</Text>
-                            </TouchableOpacity>
-                        )
-                    })}
-                </ScrollView>
-            </View>
-
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Eventos</Text>
+      </View>
+      <View style={styles.body}>
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.scrollViewContent}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
+        >
+          {hasEvents ? <Text style={styles.noEventsText}>No hay eventos</Text> : null}
+          {events.map((event) => {
+            return (
+              <TouchableOpacity
+                style={styles.eventHeader}
+                key={event.id}
+                onPress={() => {
+                  navigation.navigate('Statistics', {
+                    eventId: event.id,
+                    eventName: event.title,
+                    eventCapacity: event.capacity,
+                    eventVacancies: event.vacancies,
+                  });
+                }}
+              >
+                <Text style={styles.eventTitle}>{event.title}</Text>
+                <Text style={styles.eventDate}>{GetDayOfWeek(event.date)}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        flex: 1
-    },
-    header: {
-        backgroundColor: '#fff',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderBottomColor: '#000',
-        borderBottomWidth: 1
-    },
-    headerText: {
-        paddingTop: 40,
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    body: {
-        backgroundColor: '#fff',
-        flex: 9,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    eventContainer: {
-        backgroundColor: '#fff',
-        width: '90%',
-        borderColor: '#000',
-        borderWidth: 1,
-        marginBottom: 10
-    },
-    eventHeader: {
-        backgroundColor: '#fff',
-        flex: 1,
-        alignItems: 'center',
-        padding: 10
-    },
-    eventTitle: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    eventDate: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    noEventsText: {
-        paddingTop: 20,
-        fontSize: 20,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  headerText: {
+    paddingTop: 40,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: '#fff',
+    flex: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollViewContent: {
+    padding: 20,
+  },
+  eventHeader: {
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+  },
+  eventTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  eventDate: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  noEventsText: {
+    paddingTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
