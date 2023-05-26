40fb3d611d56dc68d1b33b47a0bfef81c1571e51
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-05-26 17:13:38 -0300

    Agrega refresh de estadisticas cada 30 segundos

--- src/screens/Options.js
+++ src/screens/Options.js
@@ -6,7 +6,6 @@ import AttendanceChart from '../components/AttendanceChart';
 import ChartExample from '../components/ChartExample';
 import { ScrollView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import GetDayOfWeek from '../libs/DayOfWeek';
-import { discovery } from 'expo-auth-session/providers/google';
 
 const getData = async () => {
   try {
@@ -29,7 +28,7 @@ export default function Options({ route, navigation }) {
   const fetchData = async () => {
     try {
       const token = await getData();
-      const response = await fetch(`https://b697-181-29-197-107.sa.ngrok.io/authorizer/event/statistics?event_id=${eventId}`, {
+      const response = await fetch(`https://b642-201-212-239-28.ngrok-free.app/authorizer/event/statistics?event_id=${eventId}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -40,11 +39,8 @@ export default function Options({ route, navigation }) {
       if (response.ok) {
         const responseData = await response.json();
         console.log("mis datos:", responseData);
-        // Retraso de 1 segundo antes de establecer los datos
-        setTimeout(() => {
-          setDatos(responseData);
-          setIsLoading(false); // Marcar el indicador de carga como falso después del retraso
-        }, 500);
+        setDatos(responseData);
+        setIsLoading(false); // Marcar el indicador de carga como falso después de recibir los datos
       } else {
         console.error('Error en la respuesta del servidor:', response.status);
       }
@@ -53,12 +49,18 @@ export default function Options({ route, navigation }) {
     }
   };
 
-
-
-
-
   useEffect(() => {
     fetchData();
+
+    // Set up an interval for automatic refresh every 30 seconds
+    const interval = setInterval(() => {
+      fetchData();
+    }, 30000);
+
+    // Clear the interval when the component is unmounted
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
@@ -68,76 +70,76 @@ export default function Options({ route, navigation }) {
       {isLoading ? (
         <LoadingSpinner /> // Mostrar el indicador de carga mientras se espera
       ) : (
-        <ChartExample datos={datos} /> // Renderizar el gráfico después del retraso
+        <ChartExample datos={datos} /> // Renderizar el gráfico después de recibir los datos
       )}
  
       <Text style={[styles.headerText]}>Asistencia</Text>
       {isLoading ? (
         <LoadingSpinner /> // Mostrar el indicador de carga mientras se espera
       ) : (
-        <AttendanceChart style={{ width: '100%' }} datos={datos} /> // Renderizar el gráfico después del retraso
+        <AttendanceChart style={{ width: '100%' }} datos={datos} /> // Renderizar el gráfico después de recibir los datos
       )}
       <Button title="Escanear entradas" key={eventId} onPress={()=>{
-                                navigation.navigate('Scan', {
-                                  eventId: eventId,
-                                  eventName: eventName,
-                                  eventCapacity: eventCapacity,
-                                  eventVacancies: eventVacancies 
-                                })
-                            }} />
+        navigation.navigate('Scan', {
+          eventId: eventId,
+          eventName: eventName,
+          eventCapacity: eventCapacity,
+          eventVacancies: eventVacancies 
+        })
+      }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        flex: 1
-    },
-    header: {
-        backgroundColor: '#fff',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderBottomColor: '#000',
-        borderBottomWidth: 1
-    },
-    headerText: {
-        paddingTop: 40,
-        fontSize: 20,
-        fontWeight: 'bold',
-        alignSelf: 'center'
-    },
-    body: {
-        backgroundColor: '#fff',
-        flex: 9,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    eventContainer: {
-        backgroundColor: '#fff',
-        width: '90%',
-        borderColor: '#000',
-        borderWidth: 1,
-        marginBottom: 10
-    },
-    eventHeader: {
-        backgroundColor: '#fff',
-        flex: 1,
-        alignItems: 'center',
-        padding: 10
-    },
-    eventTitle: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    eventDate: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    noEventsText: {
-        paddingTop: 20,
-        fontSize: 20,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    backgroundColor: '#fff',
+    flex: 1
+  },
+  header: {
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1
+  },
+  headerText: {
+    paddingTop: 40,
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+  body: {
+    backgroundColor: '#fff',
+    flex: 9,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  eventContainer: {
+    backgroundColor: '#fff',
+    width: '90%',
+    borderColor: '#000',
+    borderWidth: 1,
+    marginBottom: 10
+  },
+  eventHeader: {
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    padding: 10
+  },
+  eventTitle: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  eventDate: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  noEventsText: {
+    paddingTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
